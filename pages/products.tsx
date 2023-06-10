df37819831cdb0b87dc35458b1fef9f56e4cd0ec
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-06-10 18:10:38 +0200

    SSG & displaying products using map

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,9 +1,25 @@
+import { Product } from '@/components/product';
 import { InferGetStaticPropsType } from 'next';
 
 function ProducstPage({
   data,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{data[0].title}</div>;
+  return (
+    <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+      {data.map(product => (
+        <li key={product.id}>
+          <Product
+            data={{
+              description: product.description,
+              thumbnailUrl: product.image,
+              thumbnailAlt: product.title,
+              rating: product.rating.rate,
+            }}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ProducstPage;
